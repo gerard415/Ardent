e36c9ca5e802cd93ce1bb3cc65ebea6b0d5d49d1
gerard415 <chinedumike85@gmail.com>
2025-01-22 09:17:35 -0500

    backend clerk user set up

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,9 +6,11 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+import {clerkMiddleware, createClerkClient, requireAuth} from "@clerk/express";
 
 /* ROUTE IMPORTS */
 import courseRoutes from "./routes/courseRoute"
+import userClerkRoutes from "./routes/userClerkRoutes"
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -16,6 +18,11 @@ const isProduction = process.env.NODE_ENV === "production";
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
+
+//need to create a client to store information in clerk (settings page)
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 const app = express();
 app.use(express.json());
@@ -25,6 +32,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(clerkMiddleware());
 
 /* ROUTES */
 app.get("/", (req, res) => {
@@ -32,6 +40,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/courses", courseRoutes)
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
