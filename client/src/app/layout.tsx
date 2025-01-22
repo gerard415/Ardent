c6fc3fed6107c2f3863f9bf3b52ecefa46c95155
gerard415 <chinedumike85@gmail.com>
2025-01-22 09:18:16 -0500

    adding toaster

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import {DM_Sans} from "next/font/google"
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import { Suspense } from "react";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -26,11 +27,13 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${dmSans.className}`}>
           <Providers>
-            <div className="root-layout">{children}</div>
+            <Suspense fallback={null}>
+              <div className="root-layout">{children}</div>
+            </Suspense>
+            <Toaster richColors closeButton />
           </Providers>
         </body>
       </html>
     </ClerkProvider>
-    
   );
 }
